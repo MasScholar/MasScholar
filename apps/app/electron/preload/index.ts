27fd3f5ff9c4f8ef62414bf153892092c7e63b65
Kaifuny <16386583+Kaifuny@users.noreply.github.com>
2026-01-18 15:52:30 +0800

    chore: formate code

--- apps/app/electron/preload/index.ts
+++ apps/app/electron/preload/index.ts
@@ -1,14 +1,17 @@
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer } from 'electron'
 
 const api = {
   user: {
-    getUser: (id: string) => ipcRenderer.invoke("UserService.getUser", id),
-    updateUser: (payload: { id: string; name: string }) => ipcRenderer.invoke("UserService.updateUser", payload),
+    getUser: (id: string) => ipcRenderer.invoke('UserService.getUser', id),
+    updateUser: (payload: { id: string; name: string }) =>
+      ipcRenderer.invoke('UserService.updateUser', payload),
   },
   chat: {
-    sendMessage: (msg: { user: string; text: string }) => ipcRenderer.send("ChatService.sendMessage", msg),
+    sendMessage: (msg: { user: string; text: string }) =>
+      ipcRenderer.send('ChatService.sendMessage', msg),
   },
-};
+}
 
-contextBridge.exposeInMainWorld("NATIVE_API", api);
-export type ApiType = typeof api;
+contextBridge.exposeInMainWorld('NATIVE_API', api)
+
+export type ApiType = typeof api
--- apps/app/src/views/CreateProjectView.vue
+++ apps/app/src/views/CreateProjectView.vue
@@ -1,6 +1,4 @@
-<script lang="ts">
-</script>
+<script lang="ts"></script>
 <template>
-  <div class="flex flex-col">
-  </div>
+  <div class="flex flex-col">Create Project</div>
 </template>
