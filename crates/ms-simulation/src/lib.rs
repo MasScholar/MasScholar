3d9832482b683d100e9ae39ec2454da26af7272f
Kaifuny <16386583+Kaifuny@users.noreply.github.com>
2026-01-20 23:04:58 +0800

    feat: add ms-simulation and ms-statistics crates with initial setup
    - Created package.json for ms-simulation and ms-statistics with necessary scripts and dependencies.
    - Added rustfmt.toml to both crates to enforce consistent formatting.
    - Introduced tsconfig.json for TypeScript configuration in both crates.
    - Implemented basic test structure in ms-statistics with example tests.
    - Added benchmark scripts to measure performance of native vs JavaScript functions.
    - Configured .editorconfig, .gitattributes, .prettierignore, and .taplo.toml for consistent coding styles and formatting.

--- crates/ms-simulation/src/lib.rs
+++ crates/ms-simulation/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![deny(clippy::all)]
+
+use napi_derive::napi;
+
+#[napi]
+pub fn plus_100(input: u32) -> u32 {
+  input + 100
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[napi]
+pub fn minus_100(input: u32) -> u32 {
+  input - 100
 }
